2b37bcb0affe1d95eeca5e5851936170a930b66b
Daria Zhuromskaya <zhuromskaya_ds@mail.ru>
2023-01-07 22:53:20 +0700

    feat: section "achievements" with jquery plugins (owl, fancy)

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          700:{
+              items:2
+          },
+          900:{
+              items:3
+          },
+          1200:{
+              items:4
+          }
+      }
+  })
+});
