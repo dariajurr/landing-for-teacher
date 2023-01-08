6410b8fbc1a86eca3ec4d79208630a08b95b31e2
Daria Zhuromskaya <zhuromskaya_ds@mail.ru>
2023-01-08 17:22:26 +0700

    feat: section "my-class"

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+
+
 $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
-      loop:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          700:{
-              items:2
-          },
-          900:{
-              items:3
-          },
-          1200:{
-              items:4
-          }
-      }
-  })
+        loop:true,
+        margin: 15,
+        responsive:{
+            0:{
+                items:1
+            },
+            700:{
+                items:2
+            },
+            900:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    })
 });
